b211f17725e82b5b4fe770ce51061d1bdbbb7043
unknown <j2browns@yahoo.com>
2020-09-30 22:53:41 -0500

    extracting data to be used in bar plot

--- static/js/app.js
+++ static/js/app.js
@@ -22,13 +22,22 @@ d3.json(url).then(function(data) {
     var bbSamples = data.samples;
     console.log(bbSamples);
 
-    var subject = 955;
+    var subject = 1258;
     var dataMeta = bbMetaData.filter(data =>parseInt(data.id) === subject);
     console.log(dataMeta);
 
     var dataSamples = bbSamples.filter(data =>parseInt(data.id) === subject);
     console.log(dataSamples);
     console.log(dataSamples[0].otu_ids);
+
+    var otuIds = dataSamples[0].otu_ids.slice(0, 10);
+    console.log(otuIds);
+    var sampleValues = dataSamples[0].sample_values.slice(0, 10);
+    console.log(sampleValues);
+    var otuLabels = dataSamples[0].otu_labels.slice(0, 10);
+    console.log(otuLabels);
+
+
 });
 
 
