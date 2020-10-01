5e8a3d287bf97c1c3ef265e7262c7eadfc6b799d
unknown <j2browns@yahoo.com>
2020-09-30 23:27:55 -0500

    bar chart running with all basic features

--- static/js/app.js
+++ static/js/app.js
@@ -31,11 +31,31 @@ d3.json(url).then(function(data) {
     console.log(dataSamples[0].otu_ids);
 
     var otuIds = dataSamples[0].otu_ids.slice(0, 10);
+    var otuIdsString = otuIds.map(number => number.toString());
     console.log(otuIds);
+    console.log(otuIdsString);
     var sampleValues = dataSamples[0].sample_values.slice(0, 10);
     console.log(sampleValues);
     var otuLabels = dataSamples[0].otu_labels.slice(0, 10);
     console.log(otuLabels);
+
+    var trace1 = {
+      x: otuIdsString,
+      y: sampleValues,
+      type: "bar",
+      text: otuLabels
+    };
+
+    var data = [trace1];
+  
+  var layout = {
+    title: `Plot of Counts vs otu_id for ${subject}`,
+        xaxis: {title: "otu_id", type:"category"},
+    yaxis: {title: "counts of otu", gridwidth: 2},
+    bargap:0.05
+  };
+
+  Plotly.newPlot("bar", data, layout);
 
 
 });
